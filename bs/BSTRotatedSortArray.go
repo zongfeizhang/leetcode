ee04b116d36909c016a79568e056ac04b03f3827
geek <zhangzongfei>
2019-02-12 23:43:40 +0800

    二分查找

---
+++ bs/BSTRotatedSortArray.go
@@ -0,0 +1,27 @@
+package bs
+
+func Search(nums []int, target int) int {
+	return binarySearch(nums,0,len(nums)-1,target)
+}
+
+func binarySearch(nums []int,left,right,target int)int{
+	if left>right{
+		return -1
+	}else{
+		mid := (left + right) / 2
+		if nums[mid]==target{
+			return mid
+		}else if nums[mid]<target{
+			//位于前半段还是后半段
+			if (nums[mid]>=nums[left] && nums[mid]<=nums[right]) || (nums[mid]>=nums[left] && nums[mid]>=nums[right]) || target<=nums[right]{//连续有序
+				return binarySearch(nums,mid+1,right,target)
+			}
+			return binarySearch(nums,left,mid-1,target)
+		}else{
+			if (nums[mid]>=nums[left] && nums[mid]<=nums[right]) || (nums[mid]<=nums[left] && nums[mid]<=nums[right]) || target>nums[right]{//连续有序
+				return binarySearch(nums,left,mid-1,target)
+			}
+			return binarySearch(nums,mid+1,right,target)
+		}
+	}
+}
--- dp/MedianTwoSortedArrays.go
+++ dp/MedianTwoSortedArrays.go
@@ -1,5 +1,7 @@
 package dp
 
+import "math"
+
 func FindMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 	allLen := len(nums1) + len(nums2)
 	medians := make([]int,0)
@@ -8,54 +10,37 @@ func FindMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 	if allLen%2==0{
 		medians = append(medians,mid-1)
 	}
+
 	res := 0.0
 	for _,median := range medians{
-		result := binarySearch(nums1,0,len(nums1)-1,nums2,0,len(nums2)-1,median)
-		if result==-1{
-			result = binarySearch(nums2,0,len(nums2)-1,nums1,0,len(nums1)-1,median)
-		}
+		result := kth(nums1,0,len(nums1),nums2,0,len(nums2),median)
 		res += result
 	}
 	return res/float64(len(medians))
 }
 
-
-func binarySearch(a []int,leftA,rightA int,b []int,leftB,rightB, target int)float64{
-	if leftA>rightA{
-		return -1
+func kth(a []int,leftA,rightA int,b []int,leftB,rightB, k int)float64{
+	if leftA==rightA{
+		return float64(b[k])
 	}
-
-	mid := (leftA + rightA) / 2
-	pos := findLastLessThan(b,leftB,rightB,a[mid]) //mid会不会超范围
-	leftCount := mid + pos + 1
-	if target==leftCount{
-		return float64(a[mid])
-	}else if target==leftCount-1 && pos!=-1 && b[pos]==a[mid]{
-		return float64(b[pos])
-	}else{
-		if target<leftCount{
-			if pos==-1{
-				return float64(a[target])
-			}
-			return binarySearch(a,leftA,mid-1,b,leftB,pos-1,target)
+	if leftB==rightB{
+		return float64(a[k])
+	}
+	midA := (leftA+rightA) / 2
+	midB := (leftB+rightB) / 2
+	if midA+midB+1==k{
+		return math.Max(float64(a[midA]),float64(b[midB]))
+	}else if midA+midB+1<k{
+		if a[midA]>b[midB]{
+			return kth(a,leftA,rightA,b,midB,leftB,k-midB-1)
 		}else{
-			return binarySearch(a,mid+1,rightA,b,pos+1,rightB,target)
+			return kth(a,midA,rightA,b,leftB,rightB,k-midA-1)
 		}
-	}
-}
-
-func findLastLessThan(arr []int,left,right,value int)int{
-	if left>right{//首尾边界
-		return right
-	}
-	mid := (left+right) / 2
-	if arr[mid]>value{
-		return findLastLessThan(arr,left,mid-1,value)
 	}else{
-		if mid+1>=len(arr)||arr[mid+1]>value{
-			return mid
+		if a[midA]>b[midB]{
+			return kth(a,leftA,midA,b,leftB,rightB,k)
 		}else{
-			return findLastLessThan(arr,mid+1,right,value)
+			return kth(a,leftA,rightA,b,leftB,midB,k)
 		}
 	}
 }
