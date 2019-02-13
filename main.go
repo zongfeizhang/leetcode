224f7a1b14a64b7cc681e5eff2d4b83ca01126be
geek <zhangzongfei>
2019-02-14 00:27:31 +0800

    层次遍历

--- main.go
+++ main.go
@@ -1,39 +1,12 @@
 package main
 
 import (
+	"leetcode/bs"
 	"fmt"
-	"leetcode/dp"
 )
 
 func main() {
-	//s := "()(()"
-	//res := dp.LongestValidParentheses(s)
-	//fmt.Println(res)
-	//
-	//arr := [][]int{
-	//	{1, 3, 1},
-	//	{1, 5, 1},
-	//	{4, 2, 1},
-	//}
-	//res = dp.MinPathSum(arr)
-	//fmt.Println(res)
-	///////
-	//word1 := "intention"
-	//word2 := "execution"
-	//res = dp.MinDistance(word1, word2)
-	//fmt.Println(res)
-	//
-	//fmt.Println("=========================")
-	//s1 := "abcde"
-	//s2 := "caebd"
-	//flag := dp.IsScramble(s1, s2)
-	//fmt.Println(flag)
-	//
-	//result := dp.GenerateTrees(3)
-	//count := len(result)
-	//fmt.Println(count)
-	//
-	//fmt.Println("=============结果============")
-	n := 4
-	fmt.Println(dp.NumTrees(n))
+	nums := []int{5,7,7,8,8,10}
+	res := bs.SearchRange(nums,6)
+	fmt.Println(res)
 }
--- tree/treeLevelTraverse.go
+++ tree/treeLevelTraverse.go
@@ -1,1 +1,44 @@
 package tree
+
+type TreeNode struct {
+	Val int
+    Left *TreeNode
+    Right *TreeNode
+}
+
+func levelOrder(root *TreeNode) [][]int {
+	queue := []*TreeNode{root}
+	res := make([][]int,0)
+	if root==nil{
+		return res
+	}
+
+	for len(queue)!=0{
+		arr := fill(queue)
+		res = append(res,arr)
+		childs := bornChilds(queue)
+		queue = childs
+	}
+	return res
+}
+
+func bornChilds(parent []*TreeNode)[]*TreeNode{
+	childs := make([]*TreeNode,0)
+	for _,node := range parent{
+		if node.Left != nil{
+			childs = append(childs,node.Left)
+		}
+		if node.Right != nil{
+			childs = append(childs,node.Right)
+		}
+	}
+	return childs
+}
+
+func fill(t []*TreeNode)[]int{
+	arr := make([]int,len(t))
+	for index,node := range t{
+		arr[index] = node.Val
+	}
+	return arr
+}
